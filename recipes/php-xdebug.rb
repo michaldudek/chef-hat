32738d021449dfc4dcf8b6620f1736c366b099f0
Michał Dudek <michal@michaldudek.pl>
2015-06-30 19:49:47 +0200

    No need to symlink xdebug.ini as its already done by the package.

--- recipes/php-xdebug.rb
+++ recipes/php-xdebug.rb
@@ -11,8 +11,3 @@ template node["php"]["ext_conf_dir"] + "/xdebug.ini" do
         :config => node["php"]["xdebug"]
     })
 end
-
-link node["php"]["real_conf_dir"] + "/10-xdebug.ini" do
-    to node["php"]["ext_conf_dir"] + "/xdebug.ini"
-    only_if node["php"]["real_conf_dir"] != node["php"]["ext_conf_dir"]
-end
