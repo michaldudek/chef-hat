cffd3b9fb8ff81637d591e0c35fc3283706d9550
Michał Dudek <michal@michaldudek.pl>
2015-06-30 19:16:47 +0200

    Configure PHP with php.ini template.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,38 @@
 default["php"]["ppa"]["uri"]        = "http://ppa.launchpad.net/ondrej/php5-5.6/ubuntu"
 default["php"]["ppa"]["key"]        = "E5267A6C"
 default["php"]["ppa"]["key_server"] = "keyserver.ubuntu.com"
+default["php"]["ppa"]["components"] = ["trusty", "main"]
+default["php"]["conf_dir"]          = "/etc/php5/cli"
+default["php"]["ext_conf_dir"]      = "/etc/php5/mods-available"
+default["php"]["real_conf_dir"]     = "/etc/php5/cli/conf.d"
 
-default["php"]["modules"] = [
+default["php"]["modules"]           = [
     "php5-common",
     "php5-cli",
     "php5-memcached",
     "php5-dev"
 ]
 
-default["php"]["config"] = []
+# php.ini
+
+default["php"]["config"]["expose_php"]          = "Off"
+default["php"]["config"]["enable_dl"]           = "Off"
+
+default["php"]["config"]["max_execution_time"]  = 30
+default["php"]["config"]["max_input_time"]      = 60
+default["php"]["config"]["memory_limit"]        = "128M"
+default["php"]["config"]["upload_max_filesize"] = "50M"
+default["php"]["config"]["max_file_uploads"]    = "20"
+
+default["php"]["config"]["date.timezone"]       = "Europe/London"
+
+default["php"]["config"]["error_reporting"]     = "E_ALL & ~E_DEPRECATED & ~E_STRICT"
+default["php"]["config"]["display_errors"]      = "Off"
+default["php"]["config"]["error_log"]           = ""
+
+default["php"]["config"]["SMTP"]                = "localhost"
+default["php"]["config"]["smtp_port"]           = 25
+default["php"]["config"]["mail.add_x_header"]   = "On"
+default["php"]["config"]["sendmail_path"]       = "/usr/sbin/sendmail -t -i"
+
+default["php"]["config"]["custom"]              = []
--- recipes/php.rb
+++ recipes/php.rb
@@ -1,11 +1,9 @@
-include_recipe "apt"
-
 # add PPA repository for 5.6
 apt_repository "php5-5.6" do
     uri node["php"]["ppa"]["uri"]
     key node["php"]["ppa"]["key"]
     keyserver node["php"]["ppa"]["key_server"]
-    #components ['trusty', 'main']
+    components node["php"]["ppa"]["components"]
 end
 
 # install modules
@@ -13,4 +11,15 @@ node["php"]["modules"].each do |pkg|
     package pkg do
         action :install
     end
+end
+
+# configure
+template node["php"]["conf_dir"] + "/php.ini" do
+    source "php.ini.erb"
+    owner "root"
+    group "root"
+    mode "0644"
+    variables({
+        :config => node["php"]["config"]
+    })
 end
