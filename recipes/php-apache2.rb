f29278743a35bcf156bc5e9088124c4c8707d05a
Michał Pałys-Dudek <michal@michaldudek.pl>
2018-02-26 23:56:04 +0000

    Fix recipe name.

--- recipes/php-apache2.rb
+++ recipes/php-apache2.rb
@@ -1,7 +1,7 @@
-# require apache2 with mod_php5 to be already installed
+# require apache2 with mod_php to be already installed
 include_recipe "chef-hat::php"
 include_recipe "apache2"
-include_recipe "apache2::mod_php5"
+include_recipe "apache2::mod_php"
 
 directory "/etc/php5/apache2" do
     action :delete
